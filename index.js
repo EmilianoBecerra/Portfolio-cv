3afaf75be31732d7349459e8606ef022bacf317e
Emi Becerra <emibecerra92@gmail.com>
2025-03-07 12:03:59 -0300

    add animation

--- index.js
+++ index.js
@@ -8,11 +8,70 @@ cards.forEach((card, index) => {
 });
 
 const imgs = document.querySelectorAll(".img-tecno");
+const initialPosition = [];
 
-imgs.forEach((img) => {
+function dance() {
   for (let i = 0; i < imgs.length; i++) {
-    let random = 10;
-    img.style.transform = `translateX(${random})`;
-    random + 10;
+    let randomX = Math.floor(Math.random() * (1500 - 100) + 100);
+    let randomY = Math.floor(Math.random() * (200 - 20) + 20);
+    initialPosition.push({ x: randomX, y: randomY });
+    imgs[i].style.transform = `translate(${randomX + "px"},${randomY + "px"})`;
   }
-});
+  setTimeout(animationBubble, 1200);
+}
+
+function animationBubble() {
+  imgs.forEach((img, index) => {
+    img.animate(
+      [
+        {
+          transform: `translate(${initialPosition[index].x}px, ${initialPosition[index].y}px)`,
+        },
+        {
+          transform: `translate(${
+            initialPosition[index].x +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px, ${
+            initialPosition[index].y +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px)`,
+        },
+        {
+          transform: `translate(${
+            initialPosition[index].x +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px, ${
+            initialPosition[index].y +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px)`,
+        },
+        {
+          transform: `translate(${
+            initialPosition[index].x +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px, ${
+            initialPosition[index].y +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px)`,
+        },
+        {
+          transform: `translate(${
+            initialPosition[index].x +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px, ${
+            initialPosition[index].y +
+            Math.floor(Math.random() * (150 - -10) + -10)
+          }px)`,
+        },
+      ],
+      {
+        duration: 5000,
+        iterations: Infinity,
+        direction: "alternate",
+        easing: "ease-in",
+      }
+    );
+  });
+}
+
+dance();
