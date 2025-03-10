c05749d5d288349a7818a9e0779f4e9ebb10bd19
Emi Becerra <emibecerra92@gmail.com>
2025-03-10 13:07:00 -0300

    fix tecnicals effect

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const cards = document.querySelectorAll(".personal-card");
+const imgs = document.querySelectorAll(".img-tecno");
+const centerCircleX = 1800 * 0.5;
+const centerCircleY = 500 * 0.5;
+const radius = 200;
 
 cards.forEach((card, index) => {
   setTimeout(() => {
@@ -7,71 +11,39 @@ cards.forEach((card, index) => {
   }, index * 500);
 });
 
-const imgs = document.querySelectorAll(".img-tecno");
-const initialPosition = [];
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll >= 450) {
+    imgs.forEach((img, i) => {
+      const angle = ((2 * Math.PI) / imgs.length) * i;
+      const x = centerCircleX + radius * Math.cos(angle);
+      const y = centerCircleY + radius * Math.sin(angle);
 
-function dance() {
-  for (let i = 0; i < imgs.length; i++) {
-    let randomX = Math.floor(Math.random() * (1500 - 100) + 100);
-    let randomY = Math.floor(Math.random() * (200 - 20) + 20);
-    initialPosition.push({ x: randomX, y: randomY });
-    imgs[i].style.transform = `translate(${randomX + "px"},${randomY + "px"})`;
+      img.style.transform = `translate(${x}px , ${y}px)`;
+    });
   }
-  setTimeout(animationBubble, 1200);
-}
+});
 
-function animationBubble() {
+//animationBubble();
+/* function animationBubble() {
   imgs.forEach((img, index) => {
-    img.animate(
-      [
+    requestAnimationFrame(() => {
+      img.animate(
+        [
+          {
+            transform: `translate(${initialPosition[index].x}px, ${initialPosition[index].y}px)`,
+          },
+          {
+            transform: `translate(${initialPosition[index].x}px,${initialPosition[index].y}px)`,
+          },
+        ],
         {
-          transform: `translate(${initialPosition[index].x}px, ${initialPosition[index].y}px)`,
-        },
-        {
-          transform: `translate(${
-            initialPosition[index].x +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px, ${
-            initialPosition[index].y +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px)`,
-        },
-        {
-          transform: `translate(${
-            initialPosition[index].x +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px, ${
-            initialPosition[index].y +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px)`,
-        },
-        {
-          transform: `translate(${
-            initialPosition[index].x +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px, ${
-            initialPosition[index].y +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px)`,
-        },
-        {
-          transform: `translate(${
-            initialPosition[index].x +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px, ${
-            initialPosition[index].y +
-            Math.floor(Math.random() * (150 - -10) + -10)
-          }px)`,
-        },
-      ],
-      {
-        duration: 5000,
-        iterations: Infinity,
-        direction: "alternate",
-        easing: "ease-in",
-      }
-    );
+          duration: 3000,
+          iterations: Infinity,
+          direction: "alternate",
+          easing: "ease-in-out",
+        }
+      );
+    });
   });
-}
-
-dance();
+} */
