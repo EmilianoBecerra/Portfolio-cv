300c5bf3343b6c868ef3226e62335e4fb690f332
Emi Becerra <emibecerra92@gmail.com>
2025-03-10 16:28:02 -0300

    responsive design

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const cards = document.querySelectorAll(".personal-card");
 const imgs = document.querySelectorAll(".img-tecno");
-const centerCircleX = 1800 * 0.5;
-const centerCircleY = 500 * 0.5;
-const radius = 200;
 
 cards.forEach((card, index) => {
   setTimeout(() => {
@@ -11,39 +8,29 @@ cards.forEach((card, index) => {
   }, index * 500);
 });
 
-window.addEventListener("scroll", (event) => {
-  let scroll = this.scrollY;
-  if (scroll >= 450) {
-    imgs.forEach((img, i) => {
-      const angle = ((2 * Math.PI) / imgs.length) * i;
-      const x = centerCircleX + radius * Math.cos(angle);
-      const y = centerCircleY + radius * Math.sin(angle);
+function positionSkills() {
+  const container = document.querySelector(".div-tecnicals");
+  const containerWidth = container.clientWidth;
+  const containerHeight = container.clientHeight;
+  const centerCircleX = containerWidth * 0.5;
+  const centerCircleY = containerHeight * 0.5;
+  const radius = Math.min(containerWidth, containerHeight) / 2.5;
+  window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    if (scroll >= containerHeight / 3) {
+      imgs.forEach((img, i) => {
+        const angle = ((2 * Math.PI) / imgs.length) * i;
+        const x =
+          centerCircleX + radius * Math.cos(angle) - img.clientWidth / 2;
+        const y =
+          centerCircleY + radius * Math.sin(angle) - img.clientHeight / 2;
 
-      img.style.transform = `translate(${x}px , ${y}px)`;
-    });
-  }
-});
-
-//animationBubble();
-/* function animationBubble() {
-  imgs.forEach((img, index) => {
-    requestAnimationFrame(() => {
-      img.animate(
-        [
-          {
-            transform: `translate(${initialPosition[index].x}px, ${initialPosition[index].y}px)`,
-          },
-          {
-            transform: `translate(${initialPosition[index].x}px,${initialPosition[index].y}px)`,
-          },
-        ],
-        {
-          duration: 3000,
-          iterations: Infinity,
-          direction: "alternate",
-          easing: "ease-in-out",
-        }
-      );
-    });
+        img.style.transform = `translate(${x}px , ${y}px)`;
+      });
+    }
   });
-} */
+}
+
+
+window.addEventListener("load", positionSkills);
+window.addEventListener("resize", positionSkills);
